159ceb7a61dd62b3d355a299e3dc4588acc34e6c
thalassarche <benphalan@gmail.com>
2016-05-27 11:48:35 -0700

    Extensive updates to run models for multiple species, and multiple time periods for a species. Output is in the form of a list, with a table for each species. Next step is to join with ownership information

--- PoissonTrends.r
+++ PoissonTrends.r
@@ -7,10 +7,76 @@ rm(list = ls())
 
 # Load packages
 require(XLConnect)   # for reading Excel files
-require(dplyr)       # for joins
+require(dplyr)       # for joins, and for ldply
 require(coda)
 require(arm)
 require(lme4)        # For mixed effect models
+
+########################
+### DEFINE FUNCTIONS ###
+########################
+
+# Function to remove transects with all zero counts for a species
+# and single counts for an observer-route combination (of which, v few)
+# This second part disabled by setting to 0 (not 1) in calculation of c
+removezero <- function(d){
+  a <- aggregate(d$SpeciesTotal, list(d$StateRoute), sum)
+  names(a) <- c("StateRoute", "AllYearTotal")
+  updated <- left_join(d, a)
+  b <- updated[updated$AllYearTotal>0,]
+  routesums2 <- aggregate(b$SpeciesTotal, list(b$StateRoute, b$ObsN), length)
+  names(routesums2) <- c("StateRoute", "ObsN", "ObsRouteCounts")
+  updated2 <- left_join(b, routesums2)
+  c <- updated2[updated2$ObsRouteCounts>0,]
+  return(c)
+}
+
+# Fit model, when there is one observer
+singleobs <- function(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out, period) {
+  m1 <- NULL   # reset m1
+  try(m1 <- glm(count ~ year2, data=jags.dat, poisson(link = "log")))
+  
+  if(is.null(m1)==FALSE) try({
+    alpha <- as.numeric(coef(m1)[1])
+    beta <- as.numeric(coef(m1)[2])                           # coefficient for year
+    sd.yr <- coef(summary(m1))[2,2] * sqrt(length(year2))     # standard deviation of year (may be inaccurate)
+    vari <- sd.yr*sd.yr                                       # variance
+    fitted <- rep(0,length(year2))
+    for (t in 1:nyears) {
+      fitted[t] <- exp(alpha + beta*year2[t])  # Predictions from model
+    }
+  })
+  
+  B <- 100*((fitted[nyears]/fitted[1])^(1/(nyears-1))-1)            # Annual % trend
+  
+  out[i,1:7]  <<- c(focalsp, routelist[i], min(year)+ref.yr, max(year)+ref.yr, nyears, nobservers, mean(count))
+  out[i,8:10] <<- c(B, vari, period)
+}
+
+# Fit model, when there are multiple observers
+multiobs <- function(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out, period) {
+  m1 <- NULL   # reset m1
+  try(m1 <- glmer(count ~ year2 + (1|obser) - 1, data=jags.dat, poisson(link = "log")))
+  
+  if(is.null(m1)==FALSE) try({
+    mean.obs <- mean(unlist(ranef(m1)$obser))
+    beta <- fixef(m1)                               # coefficient for year
+    sd.obs <- as.numeric(as.data.frame(VarCorr(m1))[5])         # not: sd(unlist(ranef(m1)$obser))
+    sd.yr <- se.fixef(m1) * sqrt(length(year2))     # standard deviation of year (may be inaccurate)
+    vari <- sd.obs*sd.obs + sd.yr*sd.yr             # sum of variances
+    fitted <- rep(0,length(year2))
+    for (t in 1:nyears) {
+      fitted[t] <- exp(mean.obs + beta*year2[t] + 0.5*sd.obs*sd.obs)  # Predictions from model
+    }
+  })
+  
+  B <- 100*((fitted[nyears]/fitted[1])^(1/(nyears-1))-1)            # Annual % trend
+  
+  print(routelist[i])   # To keep track of error messages
+  
+  out[i,1:7]  <<- c(focalsp, routelist[i], min(year)+ref.yr, max(year)+ref.yr, nyears, nobservers, mean(count))
+  out[i,8:10] <<- c(B, vari, period)
+}
 
 ########################
 ### PREPARE DATA     ###
@@ -56,48 +122,182 @@ dat1$RouteObs <- paste(dat1$StateRoute, dat1$ObsN, sep="_")
 # extract data for the NWFP area transects, and where RunType=1, and where (weather) data complete
 # RunType "1" is acceptable data
 # RunType "0" is unacceptable data for USGS trend analyses purposes
-dat <- left_join(nwfp_routes, dat1)
-dat <- dat[dat$RunType==1,]
-dat <- dat[complete.cases(dat),]   # Only removes three rows
+dat2 <- left_join(nwfp_routes, dat1)
+dat2 <- dat2[dat2$RunType==1,]
+dat2 <- dat2[complete.cases(dat2),]   # Only removes three rows
 
 # add data on ownership
-dat <- left_join(dat, rd0)
+dat2 <- left_join(dat2, rd0)
 
 # define single breakpoint if using two segments (1990/1991, i.e. 1991 splits data evenly)
-breakp <- 1991
+breakp <- 1990
+
+# define time periods
+dat2$period <- paste("1968-1990")
+dat2$period[dat2$Year>breakp] <- paste("1991-2014")  # change to 2015 when I have those data
+dat2$period <- as.factor(dat2$period)
+nperiods <- length(levels(dat2$period))
 
 # select and extract only the useful columns
-seldat <- dat[,c("StateRoute","Year", "period", "Aou", "SpeciesTotal", "ObsN", "RouteObs", "RouteName",
+seldat <- dat2[,c("StateRoute","Year", "period", "Aou", "SpeciesTotal", "ObsN", "RouteObs", "RouteName",
                  "Elevation", "Latitude", "Longitude", "ForestProp", "TreeCover",
                  "PropFederal", "PropIndustry", "PropPrivate", "HasFederal", "HasPrivate")]
 
-# Extract data for each focal species 
-# see: Phalan_SpeciesTimesRoutes.xlsx
-focalsp <- "04330"
-RUHU1 <- seldat[seldat$Aou==focalsp,]
+# Define focal species  # see: Phalan_SpeciesTimesRoutes.xlsx
+focalsp.list <- c("04330", "04590", "04660", "06460", "06800", "06850", "04340", "07420", 
+                  "05990", "06150",
+                  "07630", "04641", "07590", "06680", "07480", "04240", "07260", "07410", 
+                  "04680", "07221", "04030")
+allspp.names <- c("Rufous Hummingbird", "Olive-sided Flycatcher", "Willow Flycatcher", "Orange-crowned Warbler",
+                  "MacGillivray's Warbler", "Wilson's Warbler", "Allen's Hummingbird", "Wrentit", "Lazuli Bunting",
+                  "Violet-green Swallow", "Varied Thrush", "Pacific-slope Flycatcher", "Hermit Thrush",
+                  "Townsend's Warbler", "Golden-crowned Kinglet", "Vaux's Swift", "Brown Creeper", "Chestnut-backed Chickadee",
+                  "Hammond's Flycatcher", "Pacific Wren", "Red-breasted Sapsucker")
 
-# Function to remove transects with all zero counts for a species
-# and single counts for an observer-route combination (of which, v few)
-# This second part disabled by setting to 0 (not 1) in calculation of c
-removezero <- function(d){
-  a <- aggregate(d$SpeciesTotal, list(d$StateRoute), sum)
-  names(a) <- c("StateRoute", "AllYearTotal")
-  updated <- left_join(d, a)
-  b <- updated[updated$AllYearTotal>0,]
-  routesums2 <- aggregate(b$SpeciesTotal, list(b$StateRoute, b$ObsN), length)
-  names(routesums2) <- c("StateRoute", "ObsN", "ObsRouteCounts")
-  updated2 <- left_join(b, routesums2)
-  c <- updated2[updated2$ObsRouteCounts>0,]
-  return(c)
-}
+#################################
+### RUN FOR MULTIPLE SPECIES  ###
+#################################
+
+out.allspp <- list()
+
+# Record the time
+time1 <- Sys.time()
+
+for (f in 1:length(focalsp.list)) {
+
+focalsp <- focalsp.list[f]
+spdat1 <- seldat[seldat$Aou==focalsp,]
 
 # Select years
-# e.g., remove 2014, to compare with BBS online route summaries
-RUHU <- RUHU1[RUHU1$Year<breakp,]          
-dat <- removezero(RUHU)
+# e.g., remove 2014 if wanting to compare with BBS online route summaries
+#spdat <- spdat1[spdat1$Year<2014,]          
+dat <- removezero(spdat1)          # Change to spdat if running previous line
 
-# generate list of routes
+# generate list of routes for that species
 routelist <- unique(dat$StateRoute)
+
+  #################################################
+  ### DEFINE AND RUN MODELS FOR MULTIPLE ROUTES ###
+  #################################################
+  
+  ref.yr <- 1967  # as far as I understand, this is what BBS uses
+  
+  # Set up list to store multiple dataframes
+  outlist <- list()
+  
+  for (j in 1:nperiods) {
+  
+  dat_temp <- dat[dat$period==levels(dat$period)[j],]
+    
+  # Set up dataframe to store results  
+  N <- length(unique(dat$StateRoute))
+  cols <- c("species", "StateRoute", "startyear", "endyear", "nyears",
+            "nobservers", "meancount", "B", "variance", "period")
+  out <- data.frame(matrix(0, nrow=N, ncol=length(cols)))
+  colnames(out) <- cols
+  
+  # Assumes model already defined from previous section
+  for (i in 1:length(routelist)) {
+    
+    # Select route
+    temp <- dat_temp[dat_temp$StateRoute==routelist[i],]  # Select a route
+    
+    # Define model components
+    nobservers <- length(unique(temp$ObsN))
+    count <- temp$SpeciesTotal ;   ncounts <- length(count)
+    year=temp$Year-ref.yr      ;   nyears <- length(year)
+    fixedyear <- round(nyears/2-1)  # In BBS code, fixedyear is 20
+    obser=as.numeric(factor(temp$ObsN))
+    year2 <- year-fixedyear
+    period <- temp$period[1]
+    oneobs <- nobservers==1      # Check if one observer, or more
+    
+    # Prepare the data
+    jags.dat <- list(ncounts=ncounts, nobservers=nobservers, nyears=nyears,
+                     fixedyear=fixedyear, count=count, year=year, obser=obser)
+    
+    ifelse (oneobs=="TRUE", try(singleobs(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out, period)),
+            try(multiobs(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out, period)))
+  }
+  
+  outlist[[j]] <- out
+  }
+  
+  # List to dataframe
+  out.frame <- ldply(outlist, data.frame)
+  
+  # Coerce back to numeric (the NAs coerce it to character)
+  for (i in 1:ncol(out.frame)) {
+    out.frame[,i] <- as.numeric(out.frame[,i])
+  }
+  
+  # Subset routes with meancount of at least 0.25 (1 bird/four years)
+  # and where average observer contributes at least four years of data
+  out_clean <- subset(out.frame, meancount>=0.25 & (nyears/nobservers)>=4)
+  out_clean <- out_clean[complete.cases(out_clean), ]                       # removes NaNs
+  # It may be better to produce a column for each of these and then select/weight using those
+
+out.allspp[[f]] <- out_clean
+}
+  
+# Add names to list
+names(out.allspp) <- allspp.names
+
+# Output the time taken
+Sys.time()-time1
+
+# Save out table (or out_clean) #update to save in list form
+write.table(out,file="spdat_trends_poisson.csv", sep=",")
+#out <- read.csv("spdat_trends_poisson.csv", sep=",")
+
+#######################
+### EXAMINE RESULTS ###
+#######################
+
+# Examine out.allspp
+lapply(out.allspp, function(x) tapply(x[,8], x[,10], median))
+
+# Next, join to route information and categorize by ownership #
+
+
+# Compare an example species with trends from BBS
+wdir <- getwd()
+setwd("/Users/benphalan/Dropbox/FILE_BACKUP/Projects/OregonForestsBBS/misc/Analyses/2016_01_28/data")
+bbs0 <- readWorksheetFromFile("SppBBS_Trends04330RUHU_20151203.xlsx", sheet=1, header=TRUE)
+setwd(wdir)
+
+out$StateRoute <- as.numeric(out$StateRoute)
+spdat.cf <- left_join(out_clean, bbs0, by = c("StateRoute"="RouteNum"))
+spdat.cf <- data.frame(spdat.cf)
+spdat.cf[,c(2,3,4,5,6,7,8,12,21,23,25)]   # Need to update
+
+x <- as.numeric(spdat.cf$B)
+y <- as.numeric(spdat.cf$EEQ_TrendEst)
+x.sd <- as.numeric(spdat.cf$variance)
+y.sd <- sqrt(as.numeric(spdat.cf$EEQ_Variance))
+plot(x,y, xlab="B", ylab="EEQ Trend")#, xlim=c(-30,30), ylim=c(-30,75))
+abline(0,1, lty=3, lwd=2)
+abline(v=0, lty=3, lwd=1)
+abline(h=0, lty=3, lwd=1)
+arrows(x, y-y.sd, x, y+y.sd, length=0.05, angle=90, code=3, col="grey")
+arrows(x-x.sd, y, x+x.sd, y, length=0.05, angle=90, code=3, col="grey")
+points(x,y)
+abline(lm(y~x, na.action=na.exclude))    # plot regression line
+summary(lm(y~x, na.action=na.exclude))   # check R2 of trend comparison
+
+# Data visualising
+d <- x-y
+plot(d ~ spdat.cf$meancount)
+require(ggplot2)
+cs <- spdat.cf$meancount
+bs <- spdat.cf$B
+ratioyo <- spdat.cf$nyears/spdat.cf$nobservers
+p <- qplot(ratioyo, bs, colour=cs)
+p + scale_colour_gradient(limits=c(0,0.25))
+
+### FOOTNOTES
+# BBS model from: ftp://ftpext.usgs.gov/pub/er/md/laurel/Sauer/Population%20Change%20Estimation%20Webinar/
+# JAGS e.g. adapted from POLS 506 Bonus lecture here: http://jee3.web.rice.edu/teaching.htm
 
 ###############################
 ### RUN MODEL FOR ONE ROUTE ###
@@ -141,103 +341,6 @@ for (t in 1:nyears) {
 B <- 100*((fitted[nyears]/fitted[1])^(1/(nyears-1))-1)
 B
 
-#################################################
-### DEFINE AND RUN MODELS FOR MULTIPLE ROUTES ###
-#################################################
-
-##### Define functions #####
-
-# Function for when there is one observer
-singleobs <- function(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out) {
-  m1 <- NULL   # reset m1
-  try(m1 <- glm(count ~ year2, data=jags.dat, poisson(link = "log")))
-  
-  alpha <- as.numeric(coef(m1)[1])
-  beta <- as.numeric(coef(m1)[2])                           # coefficient for year
-  sd.yr <- coef(summary(m1))[2,2] * sqrt(length(year2))     # standard deviation of year (may be inaccurate)
-  vari <- sd.yr*sd.yr                                       # variance
-  fitted <- rep(0,length(year2))
-  for (t in 1:nyears) {
-    fitted[t] <- exp(alpha + beta*year2[t])  # Predictions from model
-  }
-  B <- 100*((fitted[nyears]/fitted[1])^(1/(nyears-1))-1)            # Annual % trend
-  
-  out[i,] <<- c(focalsp, routelist[i], min(year), max(year), nyears, nobservers, mean(count), B, vari)
-}
-
-# Function for when there are multiple observers
-multiobs <- function(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out) {
-  m1 <- NULL   # reset m1
-  try(m1 <- glmer(count ~ year2 + (1|obser) - 1, data=jags.dat, poisson(link = "log")))
-  
-  mean.obs <- mean(unlist(ranef(m1)$obser))
-  beta <- fixef(m1)                               # coefficient for year
-  sd.obs <- as.numeric(as.data.frame(VarCorr(m1))[5])         # not: sd(unlist(ranef(m1)$obser))
-  sd.yr <- se.fixef(m1) * sqrt(length(year2))     # standard deviation of year (may be inaccurate)
-  vari <- sd.obs*sd.obs + sd.yr*sd.yr             # sum of variances
-  fitted <- rep(0,length(year2))
-  for (t in 1:nyears) {
-    fitted[t] <- exp(mean.obs + beta*year2[t] + 0.5*sd.obs*sd.obs)  # Predictions from model
-  }
-  B <- 100*((fitted[nyears]/fitted[1])^(1/(nyears-1))-1)            # Annual % trend
-  
-  out[i,] <<- c(focalsp, routelist[i], min(year), max(year), nyears, nobservers, mean(count), B, vari)
-}
-
-# Set up dataframe to store results
-N <- length(unique(dat$StateRoute))
-cols <- c("species", "StateRoute", "startyear", "endyear", "nyears",
-          "nobservers", "meancount", "B", "variance")
-out <- data.frame(matrix(0, nrow=N, ncol=length(cols)))
-colnames(out) <- cols
-
-# Record the time
-time1 <- Sys.time()
-
-# Assumes model already defined from previous section
-for (i in 1:length(routelist)) {
-  
-  # Select route
-  temp <- dat[dat$StateRoute==routelist[i],]  # Select a route
-  
-  # Define model components
-  nobservers <- length(unique(temp$ObsN))
-  count <- temp$SpeciesTotal ;   ncounts <- length(count)
-  year=temp$Year-1967        ;   nyears <- length(year)
-  fixedyear <- round(nyears/2-1)  # In BBS code, fixedyear is 20
-  obser=as.numeric(factor(temp$ObsN))
-  year2 <- year-fixedyear
-  oneobs <- nobservers==1      # Check if one observer, or more
-  
-  # Prepare the data
-  jags.dat <- list(ncounts=ncounts, nobservers=nobservers, nyears=nyears,
-                   fixedyear=fixedyear, count=count, year=year, obser=obser)
-  
-  ifelse (oneobs=="TRUE", try(singleobs(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out)),
-          try(multiobs(jags.dat, year2, nyears, year, focalsp, routelist, nobservers, count, out)))
-}
-
-# Output the time taken
-Sys.time()-time1
-
-# Coerce back to numeric (the NAs coerce it to character)
-for (i in 1:ncol(out)) {
-  out[,i] <- as.numeric(out[,i])
-}
-
-# Subset routes with meancount of at least 0.25 (1 bird/four years)
-# and where average observer contributes at least four years of data
-out_clean <- subset(out, meancount>=0.25 & (nyears/nobservers)>=4)
-# It may be better to produce a column for each of these and then select/weight using those
-
-# Save out table (or out_clean)
-write.table(out,file="RUHU_trends_poisson.csv", sep=",")
-#out <- read.csv("RUHU_trends_poisson.csv", sep=",")
-
-#######################
-### EXAMINE RESULTS ###
-#######################
-
 par(mfrow=c(1,1))
 
 # Visualise data
@@ -245,43 +348,4 @@ plot(count ~ year2, col=obser)
 x <- year2
 y <- count
 lines(x, fitted)
-
-# Compare with trends from BBS
-wdir <- getwd()
-setwd("/Users/benphalan/Dropbox/FILE_BACKUP/Projects/OregonForestsBBS/misc/Analyses/2016_01_28/data")
-bbs0 <- readWorksheetFromFile("SppBBS_Trends04330RUHU_20151203.xlsx", sheet=1, header=TRUE)
-setwd(wdir)
-
-out$StateRoute <- as.numeric(out$StateRoute)
-RUHU.cf <- left_join(out_clean, bbs0, by = c("StateRoute"="RouteNum"))
-RUHU.cf <- data.frame(RUHU.cf)
-RUHU.cf[,c(2,3,4,5,6,7,8,12,21,23,25)]   # Need to update
-
-x <- as.numeric(RUHU.cf$B)
-y <- as.numeric(RUHU.cf$EEQ_TrendEst)
-x.sd <- as.numeric(RUHU.cf$variance)
-y.sd <- sqrt(as.numeric(RUHU.cf$EEQ_Variance))
-plot(x,y, xlab="B", ylab="EEQ Trend")#, xlim=c(-30,30), ylim=c(-30,75))
-abline(0,1, lty=3, lwd=2)
-abline(v=0, lty=3, lwd=1)
-abline(h=0, lty=3, lwd=1)
-arrows(x, y-y.sd, x, y+y.sd, length=0.05, angle=90, code=3, col="grey")
-arrows(x-x.sd, y, x+x.sd, y, length=0.05, angle=90, code=3, col="grey")
-points(x,y)
-abline(lm(y~x, na.action=na.exclude))    # plot regression line
-summary(lm(y~x, na.action=na.exclude))   # check R2 of trend comparison
-
-# Data visualising
-d <- x-y
-plot(d ~ RUHU.cf$meancount)
-require(ggplot2)
-cs <- RUHU.cf$meancount
-bs <- RUHU.cf$B
-ratioyo <- RUHU.cf$nyears/RUHU.cf$nobservers
-p <- qplot(ratioyo, bs, colour=cs)
-p + scale_colour_gradient(limits=c(0,0.25))
-
-### FOOTNOTES
-# BBS model from: ftp://ftpext.usgs.gov/pub/er/md/laurel/Sauer/Population%20Change%20Estimation%20Webinar/
-# JAGS e.g. adapted from POLS 506 Bonus lecture here: http://jee3.web.rice.edu/teaching.htm
 
